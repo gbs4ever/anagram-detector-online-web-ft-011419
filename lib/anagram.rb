d4eb6b60c280bd043bd922115a5b419580dc6726
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-25 04:12:45 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,11 +1,15 @@
 # Your code goes here!
 class Anagram
-attr_accessor :word
+attr_accessor :word ,:name
 
 def  initialize(word)
   @word = word
 end
-
+ def match(words)
+  words.select do |word|
+     word == @word
+   end
+ end
 
 
 
